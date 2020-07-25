666b60450644dd7743545ddbd6bee48f67393e76
Todd Stevens <scarfireweb@gmail.com>
2020-07-25 13:13:44 -0400

    Added FavIcon image

--- tours.php
+++ tours.php
@@ -10,6 +10,7 @@
 <title>Dead & Company Shows</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="styles.css" rel="stylesheet" type="text/css">
+	<link rel='icon' href="images/favicon-180.png?v=2" type='image/x-cxon'
 </head>
 <body>
 	<div class="page-title">Tours</div>
